be1810002f2a0437b250cc4687e99c5a3a0bc58c
JCassio1 <Joselsondias@hotmail.com>
2023-09-08 22:15:03 +0100

    Load constructor data

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from 'react'
 import ImageLoadingSkeleton from '../components/UI/ImageLoadingSkeleton'
+import { useData } from '../hooks/http/FetchData'
+import Spinner from 'react-bootstrap/Spinner'
 
 const Homepage = () => {
+  const { data, isLoading, isError, error } = useData(
+    'https://ergast.com/api/f1/2023/constructors.json?limit=1000',
+    'data',
+    true
+  )
+
   const [isImageLoading, setIsImageLoading] = useState(true)
 
   // Image loader
@@ -23,6 +31,21 @@ const Homepage = () => {
           style={{ height: '30%', width: '20%' }}
           alt='Formula one logo'
         />
+      )}
+
+      <p style={{ marginBottom: '10px' }}>Discover the best circuit for each car brand from the F1 2023</p>
+      {isLoading && (
+        <p>
+          <strong style={{ marginRight: '12px' }}>Loading Data</strong>
+          <Spinner
+            as='span'
+            animation='grow'
+            size='sm'
+            role='status'
+            aria-hidden='true'
+            style={{ marginRight: '10px' }}
+          />
+        </p>
       )}
     </>
   )
